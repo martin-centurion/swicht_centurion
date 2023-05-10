caf15809418c6b7c45e215d9f37363b821474705
martin-centurion <martinalejandrocenturion@gmail.com>
2023-05-10 20:11:41 -0300

    [feature] create module for screen and implement home in app

---
+++ src/screens/product/index.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+const Product = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Product</Text>
+    </View>
+  );
+};
+
+export default Product;
